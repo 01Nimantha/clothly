055bbc1b5dc94d22549dd58d2963c604d6401555
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-06-26 08:13:12 +0530

    Update loging_page.dart

--- lib/pages/loging_page.dart
+++ lib/pages/loging_page.dart
@@ -1,4 +1,5 @@
 import 'package:clothly/data/google_sign_in_provider.dart';
+import 'package:clothly/pages/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
@@ -23,7 +24,7 @@ class LogingPage extends StatelessWidget {
                     "./assets/ic_launcher_foreground.png",
                   ),
                   width: 500,
-                  height: 500,
+                  height: 300,
                 ),
               ),
               Text(
@@ -41,6 +42,36 @@ class LogingPage extends StatelessWidget {
                 "Login to your account to continue",
                 style: TextStyle(
                   color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                label: Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Signin(),
+                  ));
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                label: Text(
+                  "Sign in",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
               SizedBox(
