255feeaef503a0db9161b43b31c504a0b1d1569d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-06-26 08:20:25 +0530

    update signup.dart

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -10,6 +10,11 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
