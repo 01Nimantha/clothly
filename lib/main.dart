1447d0ccb6dfe84d0480e1ded84b63f525b8e49b
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-06-26 22:09:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:clothly/data/cart_provider.dart';
 import 'package:clothly/data/filter_provider.dart';
-import 'package:clothly/data/google_sign_in_provider.dart';
 import 'package:clothly/pages/loging_page.dart';
-import 'package:clothly/pages/navigation_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
-  FirebaseFirestore.instance.settings = Settings(
-    persistenceEnabled: true,
-  );
-
   await dotenv.load(fileName: '.env');
-
-  Stripe.publishableKey = dotenv.env['publishableKey'].toString();
+  Stripe.publishableKey = dotenv.env['publishableKey']!;
   await Stripe.instance.applySettings();
 
   runApp(MyApp());
@@ -35,38 +24,12 @@ class MyApp extends StatelessWidget {
     return SafeArea(
       child: MultiProvider(
         providers: [
-          ChangeNotifierProvider(
-            create: (context) => CartProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => GoogleSignInProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => FilterProvider(),
-          ),
+          ChangeNotifierProvider(create: (_) => CartProvider()),
+          ChangeNotifierProvider(create: (_) => FilterProvider()),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          // home: NavigationPage(),
-          home: StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Something went wrong!"),
-                  );
-                }
-                if (snapshot.hasData) {
-                  return NavigationPage();
-                }
-                return LogingPage();
-                // return NavigationPage();
-              }),
+          home: LogingPage(),
         ),
       ),
     );
