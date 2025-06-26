54ce78eace143e561dcbb7ee4cdfd81736ad6f93
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-06-26 09:29:39 +0530

    update signin.dart

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Signin extends StatefulWidget {
   const Signin({super.key});
@@ -11,9 +13,102 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [],
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 153, 255, 1.0),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            child: SvgPicture.asset(
+              "assets/svg/top.svg",
+              width: 400,
+              height: 150,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                ),
+                Text(
+                  "Sign in",
+                  style: GoogleFonts.pacifico(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                    color: Color.fromRGBO(0, 153, 255, 1.0),
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email :",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "password :",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(0, 153, 255, 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(
+                  flex: 3,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
